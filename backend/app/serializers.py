5bfe66dc4700cfd99fce3637f0a62eef90dfbc16
Nash <nashruddin.amin@gmail.com>
2021-09-29 08:38:07 +0700

    Set image file size

--- backend/app/serializers.py
+++ backend/app/serializers.py
@@ -6,3 +6,8 @@ class PhotoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Photo
         fields = ['id', 'title', 'description', 'image']
+
+    def validate_image(self, image):
+        if image.size > 1048576:
+            raise serializers.ValidationError('Max size is 1MB.')
+        return image
